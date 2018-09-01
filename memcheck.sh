df5363f5eaae34066bc96d9bff4e9094256cc2b7
Dan Mercurio <dmercurio92@gmail.com>
2018-09-01 01:33:18 -0400

    Okay I'm done I swear

--- memcheck.sh
+++ memcheck.sh
@@ -14,6 +14,8 @@ MEMORY_CRITICAL_LIMIT=100 # 100mb
 MEMORY_LOW_LIMIT=300 # 300mb
 CHECK_INTERVAL='5s' # Check memory levels every 5 seconds. Run man sleep for details on possible arguments
 
+mkdir -p ~/.memcheck
+
 while true 
 do
 	# Get total free memory size in megabytes (MB) 
@@ -21,14 +23,13 @@ do
 	## check if free memory is less or equals to 100MB
 	if [[ "$free" -le "$MEMORY_CRITICAL_LIMIT"  ]]; then
 		## get top processes consuming system memory and save to temporary file 
-		ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head > /home/demo/Desktop/top_proccesses_consuming_memory.txt
-		#file=/home/demo/top_proccesses_consuming_memory.txt
-		## raise alert
+		ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head > "$HOME/.memcheck/top_proccesses_consuming_memory.txt"
+		# Raise alert
 		notify-send -u critical -t 2000 "Warning, memory is running dangerously low" "Free memory: $free MB"
 	elif [[ "$free" -le "$MEMORY_LOW_LIMIT" ]]; then
 		notify-send -u normal -t 1500 "Warning, memory is getting low" "Free memory: $free MB"
 	else
-	   echo "$free"
+	   echo "Current available memory: $free"
 	fi
 	sleep "$CHECK_INTERVAL" # Wait for next poll event
 done
